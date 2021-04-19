4e360ed9f27ba21f61f26ed9ec3a86edb1d3ae07
Aneek Basu Neogi <aneek@Aneeks-MacBook-Pro.local>
2021-04-19 02:47:16 -0400

    Initial Commit

--- config/index.mjs
+++ config/index.mjs
@@ -22,24 +22,6 @@ const config = {
   , port: process.env.SERVER_PORT
     
     // JWT secret
-  , jwtSecret: process.env.JWT_SECRET
-  , mailClientID: process.env.MAILING_SERVICE_CLIENT_ID
-  , mailClientSecret: process.env.MAILING_SERVICE_CLIENT_SECRET
-  , mailRefreshToken: process.env.MAILING_SERVICE_REFRESH_TOKEN
-  , mailSender: process.env.MAILING_SENDER_EMAIL
-  /*, host: process.env.DB_HOST
-  , user: process.env.DB_USERNAME
-  , password: process.env.DB_PASSWORD */
-  
-    // JWT expire time in seconds
-  , pwdResetLink: `${process.env.PROTOCOL}://${process.env.HOST}:${process.env.SERVER_PORT}/api/v1/resetpwd?accesstoken=`
-  , jwtExpire: parseInt(process.env.JWT_EXPIRE, 10) || 3600
-  , hostLink: `${process.env.PROTOCOL}://${process.env.HOST}`
-  , appDir: process.cwd()
-
-  // Python locations
-  , python_interpreter: process.env.PYTHON_INTERPRETER
-  , python_script: process.env.PYTHON_SCRIPT
 } 
 
 export default config
